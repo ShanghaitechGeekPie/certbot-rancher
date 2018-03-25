ceb84a6d716f9e3a6cfe8b07869606ded75c3f9b
eastpiger <lvwt@shanghaitech.edu.cn>
2018-03-26 02:42:05 +0800

    add permission protect of cloudxns.int

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -39,5 +39,6 @@ file_env 'CLOUDXNS_SECRET_KEY'
 echo \# CloudXNS API credentials used by Certbot > /opt/certbot/cloudxns.ini
 echo dns_cloudxns_api_key = $CLOUDXNS_API_KEY >> /opt/certbot/cloudxns.ini
 echo dns_cloudxns_secret_key = $CLOUDXNS_SECRET_KEY >> /opt/certbot/cloudxns.ini
+chmod 600 /opt/certbot/cloudxns.ini
 
 exec "$@"
--- makecfg.sh
+++
@@ -1,3 +0,0 @@
-echo \# CloudXNS API credentials used by Certbot > /opt/certbot/cloudxns.ini
-echo dns_cloudxns_api_key = $CLOUDXNS_API_KEY >> /opt/certbot/cloudxns.ini
-echo dns_cloudxns_secret_key = $CLOUDXNS_SECRET_KEY >> /opt/certbot/cloudxns.ini
